4d006309b9891cc1f1ba2b62b53a1076585150eb
Lucas Morais <lukazmorayz@gmail.com>
2025-12-03 23:58:52 -0300

    :sparkles: feat: Adiciona confirmação de permissão para localização

--- lib/view/MapasClinicaVeterinaria.dart
+++ lib/view/MapasClinicaVeterinaria.dart
@@ -47,6 +47,19 @@ class _MapasClinicaVeterinaria extends State<MapasClinicaVeterinaria> {
   }
 
   _localizacaoAtual() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return;
+    }
+
     Position position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
